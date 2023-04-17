4b893206d25e64b3a13ad8909ad3146165fdd50e
president <abanda435@gmail.com>
2023-04-17 13:09:54 +0200

    added preliminary code for data fetching

--- pages/index.js
+++ pages/index.js
@@ -13,4 +13,9 @@ export default function Home() {
     </div>
   )
 }
+
+// There are three ways to fetch data/props
+// 1. getStaticProps - this is done at build time
+// 2. getServerSideProps -  this allows us to fetch data on every request (slower)
+// 3. getStaticPaths - allows us to use dynamic paths based on the data we are fetching / dynamically generate paths based on data being fetched
   
