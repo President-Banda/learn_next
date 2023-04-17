acd26227268a892587a7e7a4fcb3fc4527eb8109
president <abanda435@gmail.com>
2023-04-17 12:50:00 +0200

    finished modifying header

--- components/Header.js
+++ components/Header.js
@@ -1,19 +1,20 @@
 import headerStyles from '../styles/Header.module.css'
 const Header =()=>{
-    const x = 2;
+    // const x = 2;
     return(
         <div>
-            <h1 className='title'>
+            <h1 className={headerStyles.title}>
                 <span>WebDev</span>News
             </h1>
+            <p className={headerStyles.description}>Keep up to date with the latest web dev news</p>
             {/* You might want to use this type of styling for thinsg like conditionals */}
-            <style jsx>
+            {/* <style jsx>
                 {`
                     .title{
                         color: ${ x>3? 'red': 'blue'};
                     }
                 `}
-            </style>
+            </style> */}
         </div>
     )
 }
