4fa58b5fac58413e61a70a079cc7b69386429066
president <abanda435@gmail.com>
2023-04-17 11:26:18 +0200

    added layout component

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,12 @@ import Head from 'next/head'
 export default function Home() {
   return (
     <div>
+      <Head>
+        <title>
+        Learning Is Fun
+        <meta name='keywords' content='web development, programming' />
+        </title>
+      </Head>
       <h1>Welcome To Next</h1>
     </div>
   )
