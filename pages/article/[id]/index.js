3f639fd00d4e3afd1511b7069d0f80f94a9d4b88
president <abanda435@gmail.com>
2023-04-19 23:10:37 +0200

    added static props and static paths

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -2,6 +2,7 @@ import { useRouter } from "next/router";
 import Link from "next/link";
 
 const article = ({article}) => {
+    console.log(article)
     // const router = useRouter();
     // const {id} = router.query;
 
@@ -13,7 +14,21 @@ const article = ({article}) => {
     </>
 }
 
-export const getServerSideProps = async(context) => {
+
+
+// export const getServerSideProps = async(context) => {
+//     const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`);
+
+//     const article = await res.json();
+
+//     return{
+//         props: {
+//             article
+//         }
+//     }
+// }
+
+export const getStaticProps = async(context) => {
     const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`);
 
     const article = await res.json();
@@ -22,6 +37,21 @@ export const getServerSideProps = async(context) => {
         props: {
             article
         }
+    }
+}
+
+export const getStaticPaths = async () => {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+
+    const articles = await res.json();
+
+    const ids = articles.map( (article) => article.id );
+
+    const paths = ids.map((id)=>({params:{id: id.toString() }}))
+
+    return {
+        paths,
+        fallback: false // if we go back to something that does not exist in the data , return a 4o4 page
     }
 }
 
