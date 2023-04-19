786746fa3fde0eb6954f52cebc1a97413f5d258e
president <abanda435@gmail.com>
2023-04-20 00:54:00 +0200

    added api data to main application

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -1,3 +1,4 @@
+import { server } from "../../../config";
 import { useRouter } from "next/router";
 import Link from "next/link";
 
@@ -28,8 +29,20 @@ const article = ({article}) => {
 //     }
 // }
 
+// export const getStaticProps = async(context) => {
+//     const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`);
+
+//     const article = await res.json();
+
+//     return{
+//         props: {
+//             article
+//         }
+//     }
+// }
+
 export const getStaticProps = async(context) => {
-    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`);
+    const res = await fetch( `${server}/api/articles}` );
 
     const article = await res.json();
 
