ac237aa057959a66f13fd1d9f1a4230b88cd9482
president <abanda435@gmail.com>
2023-04-19 21:43:57 +0200

    added router to receive articcles

--- components/ArticleItem.js
+++ components/ArticleItem.js
@@ -3,15 +3,15 @@ import articleStyles from '../styles/Article.module.css'
 
 const ArticleItem = ({ article }) => {
     return(
-        <Link href='article/[id]' as ={`/article/${article.id}`}>
-            <p className={articleStyles.card}>
+        <Link href="article/[id]" as ={`/article/${article.id}`}>
+            <div className={articleStyles.card}>
                 <h3>
                     {article.title} &arr;
                 </h3>
                 <p>
                     {article.body}
                 </p>
-            </p>
+            </div>
         </Link>
     )
 }
--- pages/articles/[id]/index.js
+++
@@ -1,9 +0,0 @@
-const article = () => {
-    return(
-        <div>
-            This is an article
-        </div>
-    )
-}
-
-export default article;
